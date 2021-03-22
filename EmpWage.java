6defa0ad66f7589a5cd9581ccd25b047992e03e0
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-22 12:26:26 +0530

    ADD: Wage on employee if present

--- EmpWage.java
+++ EmpWage.java
@@ -5,10 +5,13 @@ public class EmpWage
 	public static void main(String args[])
 	{
 		System.out.println("This is employee wage computation");
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int wagePerDay = wagePerHour * fullDayHour;
 		Random n = new Random();
 		int ran = n.nextInt(2);
 		if(ran == 1){
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present and wage per day is " + wagePerDay);
 		}else {
 			System.out.println("Employee is Absent");
 		}

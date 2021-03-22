fcd5650343581d28b3a2d1d1ee3d54a0ee93e4c9
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-22 13:59:15 +0530

    ADD: conditional statement for less 100 hours

--- EmpWage.java
+++ EmpWage.java
@@ -9,23 +9,34 @@ public class EmpWage
 		int partTimeHour = 4;
 		int fullDayHour = 8;
 		int totalWorkingDays = 20;
-		int wagePerDay = wagePerHour * fullDayHour;
-		int wagePartTime = wagePerHour * partTimeHour;
-		
-		for(int i=0; i<totalWorkingDays; i++){
+		int wagePerDay = 0;
+		int wagePartTime = 0;
+		int workingHours = 0;
+		int workingDays = 0;		
+
+		while ((workingDays < 20) && (workingHours < 100)){
 			Random n = new Random();
 			int ran = n.nextInt(3);
-		
+			workingDays++;			
+
 			switch(ran){
 				case 2:
+					wagePerDay = wagePerHour * fullDayHour;
+					workingHours += fullDayHour; 
 					System.out.println("Employee is Present and wage per day is " + wagePerDay);
 					break;	
 				case 1:
+					wagePartTime = wagePerHour * partTimeHour;
+					workingHours += partTimeHour;
 					System.out.println("Employee is Present for part time and wage is " + wagePartTime);
 					break;
 				case 0:
 					System.out.println("Employee is Absent");
+					break;
+				default:
+             	System.out.println("Invalid");
 			}
 		}
+		System.out.println(workingHours);
 	}
 }

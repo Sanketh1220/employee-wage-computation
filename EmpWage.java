9aed0674400e2bf5613d55c4f12917a4453f2130
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-22 13:15:47 +0530

    ADD: calculated 20 days wages

--- EmpWage.java
+++ EmpWage.java
@@ -8,20 +8,24 @@ public class EmpWage
 		int wagePerHour = 20;
 		int partTimeHour = 4;
 		int fullDayHour = 8;
+		int totalWorkingDays = 20;
 		int wagePerDay = wagePerHour * fullDayHour;
 		int wagePartTime = wagePerHour * partTimeHour;
-		Random n = new Random();
-		int ran = n.nextInt(3);
 		
-		switch(ran){
-			case 2:
-				System.out.println("Employee is Present and wage per day is " + wagePerDay);
-				break;	
-			case 1:
-				System.out.println("Employee is Present for part time and wage is " + wagePartTime);
-				break;
-			case 0:
-				System.out.println("Employee is Absent");
+		for(int i=0; i<totalWorkingDays; i++){
+			Random n = new Random();
+			int ran = n.nextInt(3);
+		
+			switch(ran){
+				case 2:
+					System.out.println("Employee is Present and wage per day is " + wagePerDay);
+					break;	
+				case 1:
+					System.out.println("Employee is Present for part time and wage is " + wagePartTime);
+					break;
+				case 0:
+					System.out.println("Employee is Absent");
+			}
 		}
 	}
 }

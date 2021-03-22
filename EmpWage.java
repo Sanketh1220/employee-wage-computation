4b4fb73e6fe8956f89ecbfe2e6eeed2193b53764
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-22 12:56:41 +0530

    ADD: Part time wage calculated

--- EmpWage.java
+++ EmpWage.java
@@ -6,13 +6,17 @@ public class EmpWage
 	{
 		System.out.println("This is employee wage computation");
 		int wagePerHour = 20;
+		int partTimeHour = 4;
 		int fullDayHour = 8;
 		int wagePerDay = wagePerHour * fullDayHour;
+		int wagePartTime = wagePerHour * partTimeHour;
 		Random n = new Random();
-		int ran = n.nextInt(2);
-		if(ran == 1){
+		int ran = n.nextInt(3);
+		if(ran == 2){
 			System.out.println("Employee is Present and wage per day is " + wagePerDay);
-		}else {
+		}else if(ran == 1) {
+			System.out.println("Employee is Present for part time and wage is " + wagePartTime);
+		}else{
 			System.out.println("Employee is Absent");
 		}
 	}

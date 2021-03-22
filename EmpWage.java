454e08c2a133824d3428d0710cd9ff9cc4715510
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-22 12:16:04 +0530

    ADD: Employee attendance

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,16 @@
+import java.util.Random;
+
 public class EmpWage 
 {
 	public static void main(String args[])
 	{
 		System.out.println("This is employee wage computation");
+		Random n = new Random();
+		int ran = n.nextInt(2);
+		if(ran == 1){
+			System.out.println("Employee is Present");
+		}else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }

65e266c981e7e945036b9466a3e9b2cc025da5dc
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-29 19:51:25 +0530

    MODIFIED: implemented the class and constructor to calculate wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,42 +1,50 @@
-import java.util.Random;
+import java.util.*;
 
-public class EmpWage 
-{
-	public static void main(String args[])
-	{
-		System.out.println("This is employee wage computation");
-		int wagePerHour = 20;
-		int partTimeHour = 4;
-		int fullDayHour = 8;
-		int totalWorkingDays = 20;
-		int wagePerDay = 0;
-		int wagePartTime = 0;
-		int workingHours = 0;
-		int workingDays = 0;		
+public class EmpWage {
 
-		while ((workingDays < 20) && (workingHours < 100)){
-			Random n = new Random();
-			int ran = n.nextInt(3);
-			workingDays++;			
+      public static final int wagePerHour = 20;
+      public static final int partTimeHour = 4;
+      public static final int fullDayHour = 8;
+      public static final int totalWorkingDays = 20;
 
-			switch(ran){
-				case 2:
-					wagePerDay = wagePerHour * fullDayHour;
-					workingHours += fullDayHour; 
-					System.out.println("Employee is Present and wage per day is " + wagePerDay);
-					break;	
-				case 1:
-					wagePartTime = wagePerHour * partTimeHour;
-					workingHours += partTimeHour;
-					System.out.println("Employee is Present for part time and wage is " + wagePartTime);
-					break;
-				case 0:
-					System.out.println("Employee is Absent");
-					break;
-				default:
-             	System.out.println("Invalid");
-			}
-		}
-		System.out.println(workingHours);
+      public static int computeWage() {
+      
+			int wagePerDay = 0;
+      	int wagePartTime = 0;
+      	int workingHours = 0;
+      	int workingDays = 0;
+
+      	while ((workingDays < 20) && (workingHours < 100)){
+         	Random n = new Random();
+         	int ran = n.nextInt(3);
+         	workingDays++;
+
+         	switch(ran){
+            	case 2:
+               	//wagePerDay = wagePerHour * fullDayHour;
+               	workingHours += fullDayHour;
+               	//System.out.println("Employee is Present and wage per day is " + wagePerDay);
+               	break;
+            	case 1:
+               	//wagePartTime = wagePerHour * partTimeHour;
+               	workingHours += partTimeHour;
+               	//System.out.println("Employee is Present for part time and wage is " + wagePartTime);
+               	break;
+            	case 0:
+               	//System.out.println("Employee is Absent");
+               	break;
+            	default:
+               	//System.out.println("Invalid");
+         	}
+			//System.out.println("This is: " + workingHours);
+      }
+      int totalEmpWage = workingHours * wagePerHour;
+		//System.out.println(workingHours);
+		System.out.println("Total employee wage: " + totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		System.out.println("This is employee wage computation");	
+		computeWage();
 	}
 }
